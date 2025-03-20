09bc113953e9a28b369d5e87a5d2ee5c1342e368
hamgyusang <ebzm00@naver.com>
2025-03-20 15:32:47 +0900

    250320 GSHAM Admin시점 영화등록 기능 구현

--- src/main/java/com/shakkib/netflixclone/repository/MovieRepository.java
+++ src/main/java/com/shakkib/netflixclone/repository/MovieRepository.java
@@ -1,13 +1,21 @@
 package com.shakkib.netflixclone.repository;
 
+import com.shakkib.netflixclone.entity.Genre;
 import com.shakkib.netflixclone.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface MovieRepository extends JpaRepository<Movie,Long> {
     //    Optional<List<Movie>> findAllByUserId(Long id);
 //    List<Movie> findAllByUserId(Long userId);
+    Optional<Movie> findFirstByTitleIgnoreCase(String title);
+
+    List<Movie> findByGenre(Genre genre);
+
+    List<Movie> findByTitleContainingIgnoreCase(String keyword);
 
 }
