28628dfa16e27bb3615b270099016ed248e4ff4c
leeingu <xltmsk@naver.com>
2025-03-24 09:49:04 +0900

    chore: 북마크 url 변경

--- src/main/java/com/shakkib/netflixclone/controllers/BookmarkController.java
+++ src/main/java/com/shakkib/netflixclone/controllers/BookmarkController.java
@@ -4,15 +4,14 @@ import com.shakkib.netflixclone.dtoes.BookMarkDTO;
 import com.shakkib.netflixclone.dtoes.CustomUserDetails;
 import com.shakkib.netflixclone.services.BookmarkService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/bookmark")
 public class BookmarkController {
 
     private final BookmarkService bookmarkService;
@@ -22,7 +21,7 @@ public class BookmarkController {
     }
 
     //북마크 등록
-    @GetMapping("/bookmark/{movieId}")
+    @GetMapping("/{movieId}")
     public ResponseEntity<?> bookmark(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable("movieId") Long movieId) {
         if(customUserDetails == null) {
             System.out.println("CustomUserDetails is null");
@@ -38,7 +37,7 @@ public class BookmarkController {
     }
 
     //북마크 삭제
-    @DeleteMapping("/bookmark/{movieId}")
+    @DeleteMapping("/{movieId}")
     public ResponseEntity<?> deleteBookmark(@AuthenticationPrincipal CustomUserDetails customUserDetails ,@PathVariable("movieId") Long movieId) {
         String email = customUserDetails.getEmail();
         if (bookmarkService.removeBookmark(email, movieId)) {
@@ -49,7 +48,7 @@ public class BookmarkController {
     }
 
     //북마크 조회
-    @GetMapping("/bookmarks")
+    @GetMapping("/list")
     public List<BookMarkDTO.Response> getAllBookmarks(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
         String email = customUserDetails.getEmail();
 
